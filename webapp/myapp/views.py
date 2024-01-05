5df40d8ae78985562e11debd80968cac7454dfbd
wzyndul <wojteckiz8630@gmail.com>
2024-01-05 18:02:14 +0100

    [ADDED] implementation for API

--- webapp/myapp/views.py
+++ webapp/myapp/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseBadRequest, HttpResponseNotFound
+from django.http import HttpResponseBadRequest, HttpResponseNotFound, JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from rest_framework import status
@@ -35,17 +35,38 @@ def delete(request, record_id):
         try:
             entry = DataEntry.objects.get(id=record_id)
         except DataEntry.DoesNotExist:
-            return HttpResponseNotFound(render(request, 'error_page.html', {'error_code': '404'}))
+            return HttpResponseNotFound(render(request, 'error_page.html', {'error_code': '404'}), status=404)
 
         entry.delete()
         return redirect('home')
 
-    return HttpResponseNotFound(render(request, 'error_page.html', {'error_code': '404'}))
+    return HttpResponseNotFound(render(request, 'error_page.html', {'error_code': '404'}), status=404)
 
 
-@api_view(['GET'])
-def api_get_data(request):
+@api_view(['GET', 'POST', 'DELETE'])
+def api_data(request, record_id=None):
     if request.method == 'GET':
         entries = DataEntry.objects.all()
         serializer = DataEntrySerializer(entries, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        serializer = DataEntrySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse({'pk': serializer.data['id']}, status=status.HTTP_201_CREATED)
+        else:
+            return JsonResponse({'error': 'Invalid data'}, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        if record_id is not None:
+            try:
+                entry = DataEntry.objects.get(id=record_id)
+            except DataEntry.DoesNotExist:
+                return JsonResponse({'error': 'Record not found'}, status=status.HTTP_404_NOT_FOUND)
+
+            entry.delete()
+            return JsonResponse({'pk': record_id}, status=status.HTTP_200_OK)
+
+
+
+
